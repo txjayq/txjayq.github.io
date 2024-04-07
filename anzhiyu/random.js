eac89a30061e42ee02753e785ef9d587bccbe69d
txjayq <2217018457@qq.com>
2024-04-07 20:47:06 +0800

    Site updated: 2024-04-07 20:47:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/02/hello-world/"];function toRandomPost(){
+var posts=["2024/04/02/Markdown 编写语法/","2024/04/02/Python 基础笔记/","2024/04/02/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
